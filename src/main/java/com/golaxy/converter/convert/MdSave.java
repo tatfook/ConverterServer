4818ec8fdbf57dd86cba541a705fd03510ebd926
YangZongze <yang_zongze@126.com>
2018-01-16 14:46:45 +0800

    修改md保存代码

--- src/main/java/com/golaxy/converter/convert/MdSave.java
+++ src/main/java/com/golaxy/converter/convert/MdSave.java
@@ -2,15 +2,22 @@ package com.golaxy.converter.convert;
 
 import com.golaxy.converter.entity.es.ESSetData;
 import com.golaxy.converter.entity.frontend.ConverterResult;
+import com.golaxy.converter.entity.mysql.Article;
+import com.golaxy.converter.exception.ConvertFailException;
 import com.golaxy.converter.exception.ExistException;
 import com.golaxy.converter.service.es.IEsService;
 import com.golaxy.converter.service.gitlab.IGitlabService;
+import com.golaxy.converter.service.kafka.IKafkaService;
 import com.golaxy.converter.service.mysql.*;
 import com.golaxy.converter.utils.CommonUtils;
 import com.golaxy.converter.utils.ContextUtil;
+import com.golaxy.converter.utils.Office2Swf;
+import org.apache.ibatis.javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Iterator;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 
 /**
  * Created by yangzongze on 2017/12/1.
@@ -19,67 +26,190 @@ import java.util.List;
  */
 public class MdSave {
 
+    private final static Logger logger = LoggerFactory.getLogger(MdSave.class);
+
     /**
-     * 保存md到gitlab
+     * 同步保存md和图片到gitlab
      * @param articleName
      * @param userName
      * @param mdList
      * @param imgList
      */
-    public static void gitlabSave(String articleName, String userName, List<ConverterResult> mdList, List<ConverterResult> imgList) {
+    public static void gitlabSave(String articleName, String userName,
+                                  List<ConverterResult> mdList, List<ConverterResult> imgList) {
 
+        if(gitlabSaveMd(articleName, userName, mdList))
+            gitlabSaveImg(imgList);
+    }
+
+    /**
+     * 同步保存md,异步保存图片
+     */
+    public static boolean gitlabSave(String articleName, String userName, String fileMd5,
+                                  List<ConverterResult> mdList, List<ConverterResult> imgList) {
+        boolean state = false;
+
+        state = gitlabSaveMd(articleName, userName, mdList);
+        if (state && imgList.size()>0)
+            gitlabSaveImgAsyn(fileMd5, imgList, mdList);
+
+        return state;
+    }
+
+    /**
+     * 同步保存md到gitlab
+     * @param articleName
+     * @param userName
+     * @param mdList
+     */
+    public static boolean gitlabSaveMd(String articleName, String userName, List<ConverterResult> mdList) {
+
+        articleName = gitlabSaveMdStep1(articleName, userName, mdList);
+        if (articleName != null)
+            return gitlabSaveMdStep2(articleName, userName, mdList);
+        else
+            return false;
+    }
+
+    /**
+     * 同步保存图片到gitlab
+     * @param imgList
+     */
+    public static void gitlabSaveImg(List<ConverterResult> imgList) {
+        IGitlabService gitlabService = (IGitlabService) ContextUtil.getBean("gitlabService");
+
+        for (ConverterResult img: imgList) {
+            String imgName = img.getName();
+            String imgPath = img.getAbsolutePath();
+
+            gitlabService.gitlabSaveImg(imgName, imgPath);
+        }
+    }
+
+    /**
+     * 异步保存图片到gitlab
+     * @param fileMd5
+     * @param imgList
+     */
+    public static void gitlabSaveImgAsyn(String fileMd5, List<ConverterResult> imgList, List<ConverterResult> mdList) {
+
+        IKafkaService kafkaService = (IKafkaService) ContextUtil.getBean("kafkaService");
+
+        Map<String, List<ConverterResult>> result = new HashMap<>();
+        result.put("img", imgList);
+        result.put("md", mdList);
+
+        try {
+            kafkaService.kafkaProduceGitlabUploadImg(fileMd5, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("upload imgs write kafka error:" + e.getMessage());
+        }
+    }
+
+    /**
+     * 第一步,先创建好md第一页的空文件，把每一页页码的markdown保存在mdList中
+     * @param articleName
+     * @param userName
+     * @param mdList
+     * @return 保存的gitlab路径下的文件夹名字
+     */
+    public static String gitlabSaveMdStep1(String articleName, String userName, List<ConverterResult> mdList) {
         IGitlabService gitlabService = (IGitlabService) ContextUtil.getBean("gitlabService");
 
         //先用第一页试探是否已存在同名文件，如果是，则存储文件夹改为 文件名（i）的形式
         String articleSrcName = articleName;
-        for(int i=0; i<mdList.size(); i++) {
-            String mdPath = mdList.get(i).getAbsolutePath();
-            String mdName = mdList.get(i).getName();
+
+        int i = 0;
+        Iterator<ConverterResult> it = mdList.iterator();
+        while (it.hasNext()) {
+            ConverterResult md = it.next();
+            String mdPath = md.getAbsolutePath();
+            String mdName = md.getName();
             if (i == 0) {
                 int j = 1;
                 while (true) {
                     try {
-                        gitlabService.gitlabSaveNone(articleName, mdName, mdPath, userName);
+                        String gitpath = gitlabService.gitlabSaveNone(articleName, mdName, mdPath, userName);
+                        if (gitpath == null)
+                            return null;
+                        String keepworkPath = gitpath.replace(".md", "").replace(GlobalVars.keepworkUserProj, "");
+                        md.setPageMd("[第"+(i+1)+"页]("+keepworkPath+") ");
                     } catch (ExistException e) {
-                        articleName = articleSrcName + "(" + j++ + ")";
+                        articleName = articleSrcName + "（" + j++ + "）";
                         continue;
                     }
                     break;
                 }
             } else {
-                try {
-                    gitlabService.gitlabSaveNone(articleName, mdName, mdPath, userName);
-                } catch (ExistException e) {
-                    e.printStackTrace();
-                }
+                String gitpath = gitlabService.getGitlabSavePath(articleName, mdName);
+                String keepworkPath = gitpath.replace(".md", "").replace(GlobalVars.keepworkUserProj, "");
+                md.setPageMd("[第"+(i+1)+"页]("+keepworkPath+") ");
             }
+            i++;
         }
+        return articleName;
+    }
 
-        for (ConverterResult img: imgList) {
-            String imgName = img.getName();
-            String imgPath = img.getAbsolutePath();
-            try {
-                gitlabService.gitlabSave(articleName, imgName, imgPath, userName, false);
-            } catch (ExistException e) {
-                e.printStackTrace();
-            }
-        }
+    /**
+     * 第二步，提交真正的md文本
+     * @param articleName
+     * @param userName
+     * @param mdList
+     */
+    public static boolean gitlabSaveMdStep2(String articleName, String userName, List<ConverterResult> mdList) {
+        IGitlabService gitlabService = (IGitlabService) ContextUtil.getBean("gitlabService");
+        boolean state = true;
 
         Iterator<ConverterResult> it = mdList.iterator();
+        int i = 1;
         while (it.hasNext()) {
             ConverterResult md = it.next();
             String mdName = md.getName();
             String mdPath = md.getAbsolutePath();
             String mdGitPath = null;
             try {
-                mdGitPath = gitlabService.gitlabSave(articleName, mdName, mdPath, userName, true);
-            } catch (ExistException e) {
+                String pageStr = "\n---\n";
+                int pageNum = mdList.size();
+                for (int j=1; j<=pageNum; j++) {
+                    if (i == j) {
+                        pageStr += "第"+i+"页 ";
+                        continue;
+                    }
+                    pageStr += mdList.get(j-1).getPageMd();
+                }
+                String fileContent = "";
+                if (pageNum > 1)
+                    fileContent = CommonUtils.read(mdPath, "UTF-8") + pageStr;
+                else
+                    fileContent = CommonUtils.read(mdPath, "UTF-8");
+                if (i == 1) {
+                    mdGitPath = gitlabService.gitlabSaveContent(articleName, mdName, fileContent, userName, true);
+                    if (mdGitPath == null) {
+                        return false;
+                    }
+                } else {
+                    try {
+                        mdGitPath = gitlabService.gitlabSaveContent(articleName, mdName, fileContent, userName, false);
+                    } catch (Exception e) {
+                        mdGitPath = gitlabService.gitlabSaveContent(articleName, mdName, fileContent, userName, true);
+                    }
+                }
+                if (pageNum > 1)
+                    CommonUtils.write(mdPath, fileContent, "UTF-8");
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            String gitlabRawUrl = GlobalVars.gitlabRawBaseUrl + mdGitPath;
-            md.setUrl(gitlabRawUrl);
-            md.setGitPath(mdGitPath);
+            if (mdGitPath != null) {
+                String gitlabRawUrl = GlobalVars.gitlabRawBaseUrl + mdGitPath;
+                md.setUrl(gitlabRawUrl);
+                md.setGitPath(mdGitPath);
+            } else {
+                state = false;
+            }
+            i++;
         }
+        return state;
     }
 
     /**
@@ -134,48 +264,67 @@ public class MdSave {
         }
     }
 
+    public static boolean mysqlCreateArticle(String articleUid, String fileMd5, String articleName,
+                                                 String userName, String userSource, Integer cateId) {
+
+        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+        ICategoryService categoryService = (ICategoryService) ContextUtil.getBean("categoryService");
+
+        Article article = articleService.getArticleByUid(articleUid);
+        if (article == null) {
+            //判断cateId是否为0，如果为0需要到MySQL中查找“未分类”ID
+            if (cateId == 0) {
+                cateId = categoryService.getCategoryId("未分类", 0, false);
+                if (cateId == 0) {
+                    // 未找到，则插入一条“未分类”记录
+                    cateId = categoryService.CategoryAdd("未分类", 0, false);
+                }
+            }
+
+            int uploadUserSourceId;
+            switch (userSource) {
+                case "tatfook":
+                    uploadUserSourceId = 0;
+                    break;
+                case "keepwork":
+                    uploadUserSourceId = 1;
+                    break;
+                default:
+                    uploadUserSourceId = 0;
+                    break;
+            }
+
+            int articleId = articleService.articleAdd(articleUid, fileMd5, articleName,
+                    uploadUserSourceId, userName, cateId, null);
+            if (articleId > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void mysqlDeleteArticle(String articleUid) {
+        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+
+        articleService.articleDel(articleUid);
+    }
+
     /**
      * 保存gitlab存储路径到MySQL
      * @param articleUid
-     * @param fileMd5
-     * @param articleName
      * @param userName
-     * @param userSource
-     * @param cateId
      * @param mdList
      */
-    public static void mysqlSaveRemote(String articleUid, String fileMd5, String articleName, String userName,
-                                 String userSource, Integer cateId, List<ConverterResult> mdList) {
+    public static boolean mysqlSaveRemoteMd(String articleUid, String userName, List<ConverterResult> mdList) {
 
-        ICategoryService categoryService = (ICategoryService) ContextUtil.getBean("categoryService");
         IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
         IMdService mdService = (IMdService) ContextUtil.getBean("mdService");
 
-        //判断cateId是否为0，如果为0需要到MySQL中查找“未分类”ID
-        if (cateId == 0) {
-            cateId = categoryService.getCategoryId("未分类", 0, false);
-            if (cateId == 0) {
-                // 未找到，则插入一条“未分类”记录
-                cateId = categoryService.CategoryAdd("未分类", 0, false);
-            }
-        }
-
-        int uploadUserSourceId;
-        switch (userSource) {
-            case "tatfook":
-                uploadUserSourceId = 0;
-                break;
-            case "keepwork":
-                uploadUserSourceId = 1;
-                break;
-            default:
-                uploadUserSourceId = 0;
-                break;
-        }
-
-        int articleId = articleService.articleAdd(articleUid, fileMd5, articleName,
-                uploadUserSourceId, userName, cateId, mdList.size());
-        if (articleId > 0) {
+        Article article = articleService.totalPageUpdate(articleUid, mdList.size());
+        if (article != null) {
             Iterator<ConverterResult> it = mdList.iterator();
             while (it.hasNext()) {
                 ConverterResult md = it.next();
@@ -183,13 +332,62 @@ public class MdSave {
                 String mdGitPath = md.getGitPath();
                 int page = md.getPage();
 
-                int mdId = mdService.mdAdd(articleId, articleUid, mdTitle, mdGitPath, page);
+                int mdId = mdService.mdAdd(article.getId(), articleUid, mdTitle, mdGitPath, page);
                 md.setMdId(mdId);
                 md.setUploadUserName(userName);
-                md.setUploadUserSourceId(uploadUserSourceId);
+                md.setUploadUserSourceId(article.getUploadUserSource());
             }
+            return true;
+        } else {
+            return false;
         }
+
     }
+
+    /**
+     * 保存gitlab存储路径到MySQL
+     * @param articleUid
+     * @param userName
+     * @param mdList
+     */
+    public static void mysqlSaveRemote(String articleUid, String md5, String articleName,
+                                       String userName, String userSource, int cateId, List<ConverterResult> mdList) {
+
+        if(mysqlCreateArticle(articleUid, md5, articleName, userName, userSource, cateId)) {
+            mysqlSaveRemoteMd(articleUid, userName, mdList);
+        }
+
+    }
+
+//    /**
+//     * 保存gitlab存储路径到MySQL
+//     * @param articleUid
+//     * @param userName
+//     * @param mdList
+//     */
+//    public static void mysqlSaveRemote(String articleUid, String md5, String articleName,
+//                                       String userName, String userSource, int cateId, List<ConverterResult> mdList) {
+//
+//
+//        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+//        IMdService mdService = (IMdService) ContextUtil.getBean("mdService");
+//
+//        Article article = articleService.totalPageUpdate(articleUid, mdList.size());
+//        if (article != null) {
+//            Iterator<ConverterResult> it = mdList.iterator();
+//            while (it.hasNext()) {
+//                ConverterResult md = it.next();
+//                String mdTitle = md.getName();
+//                String mdGitPath = md.getGitPath();
+//                int page = md.getPage();
+//
+//                int mdId = mdService.mdAdd(article.getId(), articleUid, mdTitle, mdGitPath, page);
+//                md.setMdId(mdId);
+//                md.setUploadUserName(userName);
+//                md.setUploadUserSourceId(article.getUploadUserSource());
+//            }
+//        }
+//    }
 
     public static void esSave(String articleUid, List<ConverterResult> mdList) {
 
@@ -198,7 +396,6 @@ public class MdSave {
         Iterator<ConverterResult> it = mdList.iterator();
         while (it.hasNext()) {
             ConverterResult md = it.next();
-
             try {
                 ESSetData esSetData = new ESSetData();
                 esSetData.setArticle_uid(articleUid);
@@ -219,6 +416,19 @@ public class MdSave {
                 e.printStackTrace();
             }
         }
+
+        mysqlUpdateEsIndexStatus(articleUid,true);
+    }
+
+    public static void esSaveAsyn(String articleUid, List<ConverterResult> mdList) {
+        IKafkaService kafkaService = (IKafkaService) ContextUtil.getBean("kafkaService");
+
+        try {
+            kafkaService.kafkaProduceIndexMd(articleUid, mdList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("upload imgs write kafka error:" + e.getMessage());
+        }
     }
 
     public static void mysqlUpdateEsId(int mdId, String mdUid) {
@@ -226,6 +436,86 @@ public class MdSave {
         IMdService mdService = (IMdService) ContextUtil.getBean("mdService");
 
         mdService.indexStatusUpdate(mdId, mdUid);
+    }
+
+    public static void mysqlUpdateEsIndexStatus(String articleUid, boolean status) {
+
+        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+
+        articleService.indexStatusUpdate(articleUid, status);
+    }
+
+    public static boolean getArticleIndexStatus(String articleUid) throws NotFoundException {
+        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+        return articleService.getIndexStatus(articleUid);
+    }
+
+    /**
+     * swf转换
+     * @param md5
+     * @return swf路径
+     */
+    public static String swfConvert(String md5) {
+        IFileService fileService = (IFileService) ContextUtil.getBean("fileService");
+
+        com.golaxy.converter.entity.mysql.File file = fileService.getFileByUid(md5);
+        if (file == null)
+            return null;
+
+        boolean swfConvertRunning = file.getSwfConvertRunning();
+        String swfPath = file.getSwfPath();
+        if (swfPath==null || swfPath.equals("")) {
+            String inputFilePath = GlobalVars.uploadRootPath + "/" + file.getPath();
+
+            String inputPdfFilePath = inputFilePath.replace("."+CommonUtils.getFileExt(inputFilePath), ".pdf");
+            if (new File(inputPdfFilePath).exists())
+                inputFilePath = inputPdfFilePath;
+
+            if (!swfConvertRunning) {
+                fileService.swfPathUpdate(md5, true, null);
+                try {
+                    String outFilePath = Office2Swf.office2Swf(inputFilePath, null);
+                    swfPath = CommonUtils.getRelativePath(GlobalVars.uploadRootPath, outFilePath);
+                    fileService.swfPathUpdate(md5, false, swfPath);
+                } catch (ConvertFailException e) {
+                    e.printStackTrace();
+                    fileService.swfPathUpdate(md5, false, null);
+                }
+            } else {
+                do {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    file = fileService.getFileByUid(md5);
+                    swfConvertRunning = file.getSwfConvertRunning();
+                    swfPath = file.getSwfPath();
+                } while (swfConvertRunning);
+            }
+        }
+
+        return swfPath;
+    }
+
+    public static int getConvertState(String md5) {
+        IFileService fileService = (IFileService) ContextUtil.getBean("fileService");
+
+        com.golaxy.converter.entity.mysql.File file = fileService.getFileByUid(md5);
+        if (file == null)
+            return -1;
+
+        return file.getConvertStatus();
+    }
+
+    public static boolean mysqlArticleExist(String md5, String uid) {
+        IArticleService articleService = (IArticleService) ContextUtil.getBean("articleService");
+
+        Article article = articleService.getArticleByUid(uid);
+        if (article == null)
+            return false;
+        else
+            return true;
     }
 
 }
