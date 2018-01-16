1ea2579b685b5c2442599ac2f63bda662eeab206
YangZongze <yang_zongze@126.com>
2018-01-16 15:44:17 +0800

    修改kafka操作

--- src/main/java/com/golaxy/converter/service/kafka/IKafkaService.java
+++ src/main/java/com/golaxy/converter/service/kafka/IKafkaService.java
@@ -1,6 +1,10 @@
 package com.golaxy.converter.service.kafka;
 
+import com.golaxy.converter.entity.frontend.ConverterResult;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.List;
+import java.util.Map;
 
 
 public interface IKafkaService {
@@ -8,4 +12,9 @@ public interface IKafkaService {
 	public KafkaConsumer<String, String> kafkaConsumerInit(String topic);
 
 	public boolean kafkaProduce(String uid, String fileMd5, String userName, String uploadFileName, String userSource, int cateId) throws Exception;
+
+	public boolean kafkaProduceGitlabUploadImg(String fileMd5, Map<String, List<ConverterResult>> result) throws Exception;
+
+    public boolean kafkaProduceIndexMd(String articleUid, List<ConverterResult> mdList) throws Exception;
+
 }
--- src/main/java/com/golaxy/converter/service/kafka/impl/KafkaServiceImpl.java
+++ src/main/java/com/golaxy/converter/service/kafka/impl/KafkaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.golaxy.converter.service.kafka.impl;
 
 import com.golaxy.converter.convert.GlobalVars;
+import com.golaxy.converter.entity.frontend.ConverterResult;
 import com.golaxy.converter.entity.frontend.ResponseResult;
 import com.golaxy.converter.entity.frontend.StatusCode;
 import com.golaxy.converter.entity.kafka.KafkaUserFile;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Future;
 
@@ -30,7 +33,7 @@ public class KafkaServiceImpl implements IKafkaService {
 
 	public static Producer<String, String> kafkaProducerInit() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
+		props.put("bootstrap.servers", GlobalVars.kafkaServer);
     	props.put("acks", "all");
     	props.put("retries", 0);
     	props.put("batch.size", 16384);
@@ -45,7 +48,7 @@ public class KafkaServiceImpl implements IKafkaService {
 	@Override
 	public KafkaConsumer<String, String> kafkaConsumerInit(String topic) {
 		Properties props = new Properties();
-	    props.put("bootstrap.servers", "localhost:9092");
+	    props.put("bootstrap.servers", GlobalVars.kafkaServer);
 	    props.put("group.id", "dafu");
 	    props.put("enable.auto.commit", "true");
 	    props.put("auto.commit.interval.ms", "1000");
@@ -90,10 +93,12 @@ public class KafkaServiceImpl implements IKafkaService {
             new Callback() {
                 public void onCompletion(RecordMetadata metadata, Exception e) {
                     if (e != null) {
-                        logger.info("[Kafka]: 写入失败: "+e.getMessage());
+                        logger.error("[Kafka]: <格式转换> 写入失败 | md5: "+fileMd5+" | uid: " + uid
+                                +" | 文件名:" + userFile.getFileName() +" | errMsg: "+e.getMessage());
                         SessionHandler.closeSession(uid);
                     } else {
-                        logger.info("[Kafka]: 写入成功   offset:" + metadata.offset() + " | uid:" + uid + " | 文件名:" + userFile.getFileName());
+                        logger.info("[Kafka]: <格式转换> 写入成功 | offset: " + metadata.offset()
+                                + " | md5: "+fileMd5+" | uid: " + uid + " | 文件名:" + userFile.getFileName());
                         // offset写入session
                         WebSocketSession session = SessionHandler.addAttr(uid, "offset", String.valueOf(metadata.offset()));
                         // 把该session存入该topic对应的session组中
@@ -125,4 +130,79 @@ public class KafkaServiceImpl implements IKafkaService {
 
         return offset>0 ? true : false;
     }
+
+    /**
+     * 提交kafka上传图片到gitlab队列
+     *
+     * @param fileMd5
+     * @param result
+     */
+    @Override
+    public boolean kafkaProduceGitlabUploadImg(String fileMd5, Map<String, List<ConverterResult>> result) throws Exception {
+
+        String topic = "imgUpload";
+
+        Future<RecordMetadata> future = producer.send(new ProducerRecord<String, String>(topic, fileMd5, JackJsonUtils.toJson(result)),
+                new Callback() {
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        if (e != null) {
+                            logger.info("[Kafka]: <图片提交gitlab> 写入失败 | errMsg: " + e.getMessage());
+                        } else {
+                            logger.info("[Kafka]: <图片提交gitlab> 写入成功 | offset: " + metadata.offset() + " | md5: " + fileMd5);
+                        }
+                    }
+                });
+
+        int offset = -1;
+        boolean flag = true;
+        while(flag){
+            if (future.isDone()) {
+                flag = false;
+                if (!future.isCancelled()) {
+                    RecordMetadata metadata = future.get();
+                    offset = (int) metadata.offset();
+                }
+            }
+        }
+
+        return offset>0 ? true : false;
+    }
+
+    /**
+     * 提交kafka md索引 队列
+     *
+     * @param articleUid
+     * @param mdList
+     */
+    @Override
+    public boolean kafkaProduceIndexMd(String articleUid, List<ConverterResult> mdList) throws Exception {
+
+        String topic = "mdIndex";
+
+        Future<RecordMetadata> future = producer.send(new ProducerRecord<String, String>(topic, articleUid, JackJsonUtils.toJson(mdList)),
+                new Callback() {
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        if (e != null) {
+                            logger.error("[Kafka]: <md提交索引> 写入失败: "+ e.getMessage());
+                        } else {
+                            logger.info("[Kafka]: <md提交索引> 写入成功  | offset:" + metadata.offset() + " | uid: " + articleUid);
+                        }
+                    }
+                });
+
+        int offset = -1;
+        boolean flag = true;
+        while(flag){
+            if (future.isDone()) {
+                flag = false;
+                if (!future.isCancelled()) {
+                    RecordMetadata metadata = future.get();
+                    offset = (int) metadata.offset();
+                }
+            }
+        }
+
+        return offset>0 ? true : false;
+    }
+
 }
