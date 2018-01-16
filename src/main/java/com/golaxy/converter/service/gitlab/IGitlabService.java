fadff570c43a7968d399cfc6ab27910974f5e303
YangZongze <yang_zongze@126.com>
2018-01-16 15:43:16 +0800

    修正gitlab操作bug

--- src/main/java/com/golaxy/converter/service/gitlab/IGitlabService.java
+++ src/main/java/com/golaxy/converter/service/gitlab/IGitlabService.java
@@ -3,8 +3,14 @@ package com.golaxy.converter.service.gitlab;
 import com.golaxy.converter.exception.ExistException;
 
 public interface IGitlabService {
+
+    public String gitlabSaveImg(String fileName, String filePath);
 	
 	public String gitlabSave(String articleName, String fileName, String filePath, String userName, boolean update) throws ExistException;
+
+	public String gitlabSaveContent(String articleName, String fileName, String fileContent, String userName, boolean update) throws ExistException;
+
+    public String gitlabSaveContent(String fileGitPath, String fileContent, String userName, boolean update) throws ExistException;
 	
 	public String gitlabSaveNone(String articleName, String fileName, String filePath, String userName) throws ExistException;
 
@@ -13,6 +19,8 @@ public interface IGitlabService {
 	public boolean gitlabUpdate(String filePath, String fileGitPath, String fileBase64Content, String userName);
 
 	public String gitlabGet(String fileGitPath) throws Exception;
+
+	public String getGitlabSavePath(String articleName, String fileName);
 
 }
 
--- src/main/java/com/golaxy/converter/service/gitlab/impl/GitlabServiceImpl.java
+++ src/main/java/com/golaxy/converter/service/gitlab/impl/GitlabServiceImpl.java
@@ -4,6 +4,7 @@ import com.golaxy.converter.entity.gitlab.GitlabGetData;
 import com.golaxy.converter.entity.gitlab.GitlabSetData;
 import com.golaxy.converter.exception.ExistException;
 import com.golaxy.converter.service.gitlab.IGitlabService;
+import com.golaxy.converter.utils.Base64Util;
 import com.golaxy.converter.utils.CommonUtils;
 import com.golaxy.converter.utils.HttpRequestUtils;
 import com.golaxy.converter.utils.JackJsonUtils;
@@ -18,8 +19,8 @@ import sun.misc.BASE64Decoder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.*;
+import java.util.concurrent.TimeoutException;
 
 @Service("gitlabService")
 public class GitlabServiceImpl implements IGitlabService {
@@ -51,19 +52,43 @@ public class GitlabServiceImpl implements IGitlabService {
 		}
 	}
 
+    @Override
+    public String gitlabSaveImg(String fileName, String filePath) {
+        String CommonPath = gitlabBasePath + "/" + CommonUtils.getStringDate() + "/images/";
+        CommonPath = CommonPath.replaceAll("[/]{2,}", "/");
+        String file_ext = fileName.substring(fileName.indexOf("."));
+        String fileGitPath = CommonPath + CommonUtils.generateFileName() + file_ext;
+        String fileBase64Content = Base64Util.file2Base64(filePath);
+        boolean state;
+
+        while(true) {
+            try {
+                state = gitlabAdd(fileName, fileGitPath, fileBase64Content, "");
+                break;
+            } catch (ExistException e) {
+                fileGitPath = CommonPath + CommonUtils.generateFileName() + file_ext;
+            }
+        }
+        if (!state)
+            return null;
+        else
+            return fileGitPath;
+    }
+
 	@Override
-	public String gitlabSave(String articleName, String fileName, String filePath, String userName, boolean update) throws ExistException {
+	public String gitlabSave(String articleName, String fileName, String filePath,
+                             String userName, boolean update) throws ExistException {
 
         String fileGitPath = gitlabBasePath + "/" + CommonUtils.getStringDate() + "/" + articleName + "/" + fileName;
-		String fileBase64Content = CommonUtils.file2Base64(filePath);
+		String fileBase64Content = Base64Util.file2Base64(filePath);
 		boolean state;
 
 		fileGitPath = fileGitPath.replaceAll("[/]{2,}", "/");
 		try {
 			if (!update)
-				state = gitlabAdd(filePath, fileGitPath, fileBase64Content, userName);
+				state = gitlabAdd(fileName, fileGitPath, fileBase64Content, userName);
 			else
-				state = gitlabUpdate(filePath, fileGitPath, fileBase64Content, userName);
+				state = gitlabUpdate(fileName, fileGitPath, fileBase64Content, userName);
 		} catch (ExistException e) {
 			throw new ExistException();
 		}
@@ -73,24 +98,79 @@ public class GitlabServiceImpl implements IGitlabService {
 			return fileGitPath;
 	}
 
-	@Override
-	public String gitlabSaveNone(String articleName, String fileName, String filePath, String userName) throws ExistException {
+    @Override
+    public String gitlabSaveContent(String articleName, String fileName, String fileContent,
+                                    String userName, boolean update) throws ExistException {
 
         String fileGitPath = gitlabBasePath + "/" + CommonUtils.getStringDate() + "/" + articleName + "/" + fileName;
-		boolean state;
-		try {
-			state = gitlabAdd(filePath, fileGitPath, "", userName);
-		} catch (ExistException e) {
-			throw new ExistException();
-		}
-		if (!state)
-			return null;
-		else
-			return fileGitPath;
-	}
+        String fileBase64Content = Base64Util.str2Base64(fileContent);
+        boolean state;
+
+        fileGitPath = fileGitPath.replaceAll("[/]{2,}", "/");
+        try {
+            if (!update)
+                state = gitlabAdd(fileName, fileGitPath, fileBase64Content, userName);
+            else
+                state = gitlabUpdate(fileName, fileGitPath, fileBase64Content, userName);
+        } catch (ExistException e) {
+            throw new ExistException();
+        }
+        if (!state)
+            return null;
+        else
+            return fileGitPath;
+    }
+
+    @Override
+    public String gitlabSaveContent(String fileGitPath, String fileContent,
+                                    String userName, boolean update) throws ExistException {
+
+        String fileBase64Content = Base64Util.str2Base64(fileContent);
+        boolean state;
+
+        fileGitPath = fileGitPath.replaceAll("[/]{2,}", "/");
+        try {
+            if (!update)
+                state = gitlabAdd(fileGitPath, fileGitPath, fileBase64Content, userName);
+            else
+                state = gitlabUpdate(fileGitPath, fileGitPath, fileBase64Content, userName);
+        } catch (ExistException e) {
+            throw new ExistException();
+        }
+        if (!state)
+            return null;
+        else
+            return fileGitPath;
+    }
 
 	@Override
-	public boolean gitlabAdd(String filePath, String fileGitPath, String fileBase64Content, String userName) throws ExistException {
+    public String gitlabSaveNone(String articleName, String fileName, String filePath, String userName) throws ExistException {
+
+        String fileGitPath = gitlabBasePath + "/" + CommonUtils.getStringDate() + "/" + articleName + "/" + fileName;
+        boolean state = false;
+
+        try {
+            state = gitlabAdd(filePath, fileGitPath, "", userName);
+        } catch (ExistException e) {
+            throw new ExistException();
+        }
+
+        if (!state)
+            return null;
+        else
+            return fileGitPath;
+    }
+
+    @Override
+    public String getGitlabSavePath(String articleName, String fileName) {
+
+        String fileGitPath = gitlabBasePath + "/" + CommonUtils.getStringDate() + "/" + articleName + "/" + fileName;
+
+        return fileGitPath;
+    }
+
+	@Override
+	public boolean gitlabAdd(String fileName, String fileGitPath, String fileBase64Content, String userName) throws ExistException {
 		
 		// gitlab接口地址
 		String url = gitlabBaseUrl + fileGitPath;
@@ -106,49 +186,87 @@ public class GitlabServiceImpl implements IGitlabService {
 		gitlabData.setContent(fileBase64Content);
 		
 		String body = JackJsonUtils.toJson(gitlabData);
-		
-		try {
-			boolean saveStatus;
-			int retryTimes = 1;
-			do {
-				Map<String, Object> result = HttpRequestUtils.httpPost(url, headers, body, false);
-				String body1 = result.get("body").toString().replaceAll("\n", "").trim();
-				if ( (int) result.get("code") > 300 ) {
-					if (body1.contains("already exists")) {
-						// {"message":"A file with this name already exists"}
-						logger.error("Gitlab | " + filePath + " | " + body1);
-						throw new ExistException();
-					} else if (body1.contains("refresh") || body1.contains("try again") || body1.contains("Branch diverged")) {
-						// {"message":"Could not update branch master. Please refresh and try again."}
-						// {"message":"Branch diverged"}
-						saveStatus = false;
-						logger.error("Gitlab | " + filePath + " | " + body1 + " | retrying times:" + retryTimes++);
-						try {
-							Thread.sleep(500);
-						} catch (InterruptedException e1) {
-							e1.printStackTrace();
-						}
-						continue;
-					} else {
-						logger.error("Gitlab | " + filePath + " | " + body1);
-						return false;
-					}
-				}
-				logger.info("Gitlab | " + filePath + " | " + body1);
-				break;
-			} while (!saveStatus);
-			
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			logger.error("Gitlab | " + filePath + " | " + e.getMessage());
-			return false;
-		} 
+
+        boolean saveStatus;
+        int retryTimes = 1;
+        int timeoutTimes = 0;
+        int serverErrorTimes = 0;
+        int forbiddenTimes = 0;
+        do {
+            Map<String, Object> result;
+            try {
+                result = HttpRequestUtils.httpPost(url, headers, body, false);
+                timeoutTimes = 0;
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+                logger.error("Gitlab | " + fileName + " | " + e.getMessage());
+                if (timeoutTimes++ > 2)
+                    return false;
+                saveStatus = false;
+                continue;
+            }
+            String body1 = result.get("body").toString().replaceAll("\n", "").trim();
+            if ( (int) result.get("code") > 300 ) {
+                if (body1.contains("already exists")) {
+                    // {"message":"A file with this name already exists"}
+                    logger.error("Gitlab | " + fileName + " | " + body1);
+                    throw new ExistException();
+                } else if (body1.contains("refresh") || body1.contains("try again") || body1.contains("Branch diverged")) {
+                    // {"message":"Could not update branch master. Please refresh and try again."}
+                    // {"message":"Branch diverged"}
+                    logger.error("Gitlab | " + fileName + " | " + body1 + " | retrying times:" + retryTimes++);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    serverErrorTimes = 0;
+                    saveStatus = false;
+                    continue;
+                } else if (body1.contains("not") || body1.contains("500 Internal Server Error")){
+                    // GitLab is not responding
+                    // {"message":"500 Internal Server Error"}
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (serverErrorTimes++ > 2) {
+                        logger.error("Gitlab | 服务器内部错误 | " + body1);
+                        return false;
+                    }
+                    saveStatus = false;
+                    continue;
+                } else if (body1.contains("Forbidden")){
+                    // {"message":"403 Forbidden"}
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (forbiddenTimes++ > 2) {
+                        logger.error("Gitlab | 服务器拒绝 | " + body1);
+                        return false;
+                    }
+                    saveStatus = false;
+                    continue;
+                } else {
+                    logger.error("Gitlab | " + fileName + " | " + body1);
+                    serverErrorTimes = 0;
+                    saveStatus = false;
+                    continue;
+                }
+
+            }
+            logger.info("Gitlab | " + fileName + " | " + body1);
+            break;
+        } while (!saveStatus);
 		
 		return true;
 	}
 
 	@Override
-	public boolean gitlabUpdate(String filePath, String fileGitPath, String fileBase64Content, String userName) {
+	public boolean gitlabUpdate(String fileName, String fileGitPath, String fileBase64Content, String userName) {
 		
 		// gitlab接口地址
 		String url = gitlabBaseUrl + fileGitPath;
@@ -165,38 +283,74 @@ public class GitlabServiceImpl implements IGitlabService {
 		
 		String body = JackJsonUtils.toJson(gitlabData);
 		
-		try {
-			boolean saveStatus = true;
-			int retryTimes = 1;
-			do {
-				Map<String, Object> result = HttpRequestUtils.httpPut(url, headers, body, false);
-				String body1 = result.get("body").toString().replaceAll("\n", "").trim();
-				if ( (int) result.get("code") > 300 ) {
-					if (body1.contains("already exists")) {//已存在
-						logger.error("Gitlab | " + filePath + " | " + body1);
-						throw new ExistException();
-					} else if (body1.contains("refresh") || body1.contains("try again") || body1.contains("Branch diverged")) {
-						saveStatus = false;
-						logger.error("Gitlab | " + filePath + " | " + body1 + " | retrying times:" + (retryTimes++));
-						try {
-							Thread.sleep(300);
-						} catch (InterruptedException e1) {
-							e1.printStackTrace();
-						}
-						continue;
-					} else {
-						logger.error("Gitlab | " + filePath + " | " + body1);
-						return false;
-					}
-				}
-				logger.info("Gitlab | " + filePath + " | " + body1);
-				break;
-			} while (!saveStatus);
-		} catch (Exception e) {
-			logger.error("Gitlab | " + filePath + " | " + e.getMessage());
-			return false;
-		} 
-		
+
+        boolean saveStatus = true;
+        int retryTimes = 1;
+        int timeoutTimes = 0;
+        int serverErrorTimes = 0;
+        int forbiddenTimes = 0;
+        do {
+            Map<String, Object> result;
+            try {
+                result = HttpRequestUtils.httpPut(url, headers, body, false);
+                timeoutTimes = 0;
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+                logger.error("Gitlab | " + fileName + " | " + e.getMessage());
+                if (timeoutTimes++ > 2)
+                    return false;
+                saveStatus = false;
+                continue;
+            }
+            String body1 = result.get("body").toString().replaceAll("\n", "").trim();
+            if ( (int) result.get("code") > 300 ) {
+                if (body1.contains("already exists")) {
+                    logger.error("Gitlab | " + fileName + " | " + body1);
+                } else if (body1.contains("refresh") || body1.contains("try again") || body1.contains("Branch diverged")) {
+                    logger.error("Gitlab | " + fileName + " | " + body1 + " | retrying times:" + (retryTimes++));
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    serverErrorTimes = 0;
+                    saveStatus = false;
+                    continue;
+                } else if (body1.contains("not") || body1.contains("500 Internal Server Error")){
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (serverErrorTimes++ > 2) {
+                        logger.error("Gitlab | 服务器内部错误 | " + body1);
+                        return false;
+                    }
+                    saveStatus = false;
+                    continue;
+                } else if (body1.contains("Forbidden")){
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    if (forbiddenTimes++ > 2) {
+                        logger.error("Gitlab | 服务器拒绝 | " + body1);
+                        return false;
+                    }
+                    saveStatus = false;
+                    continue;
+                } else {
+                    logger.error("Gitlab | " + fileName + " | " + body1);
+                    serverErrorTimes = 0;
+                    saveStatus = false;
+                    continue;
+                }
+            }
+            logger.info("Gitlab | " + fileName + " | " + body1);
+            break;
+        } while (!saveStatus);
+
 		return true;
 	}
 
