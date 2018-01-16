c0a17b9f8fe84b5bd441db75713ff7207afbd803
YangZongze <yang_zongze@126.com>
2018-01-16 15:57:29 +0800

    增加解析json为对象列表功能

--- src/main/java/com/golaxy/converter/utils/JackJsonUtils.java
+++ src/main/java/com/golaxy/converter/utils/JackJsonUtils.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 /**
  * Created by yangzongze on 2017/12/1.
@@ -35,6 +36,27 @@ public class JackJsonUtils {
             e.printStackTrace();  
         }  
         return null; 
+	}
+
+	/**
+	 * 解析json为对象列表
+	 *
+	 * @param content
+	 * @param valueTypeRef
+	 * @return
+	 */
+	public static <T> T fromJson(String content, TypeReference valueTypeRef) {
+		if (objectMapper == null) {
+			objectMapper = new ObjectMapper();
+			objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+			objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+		}
+		try {
+			return objectMapper.readValue(content, valueTypeRef);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	/** 
